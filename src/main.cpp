faac9bf61f8b9d1a7c721e063fd5cfcd1595d418
MateuszTk <mateusz.tkaczewski@gmail.com>
2021-08-08 22:59:57 +0200

    epic voxel chunks

--- src/main.cpp
+++ src/main.cpp
@@ -156,7 +156,7 @@ void rotateWorld(glm::vec3* vec) {
 
 }
 
-void draw(int width, int height, byte* img, Region* region, glm::vec3* origin, glm::vec3* direction, byte* octree, const int octree_depth, const int _csize) {
+void draw(int width, int height, byte* img, Region* region, glm::vec3* origin, glm::vec3* direction, byte* wholeoctree, const int octree_depth, const int _csize, const int chunk_count, const int chunk_size) {
 	float sx = 1.0f / (float)width;
 	float sy = 1.0f / (float)height;
 
@@ -193,113 +193,132 @@ void draw(int width, int height, byte* img, Region* region, glm::vec3* origin, g
 			ray.sign[1] = (ray.invdir.y < 0);
 			ray.sign[2] = (ray.invdir.z < 0);
 
-			float dist = 0xffffff;
-			byte oc = 255;
-			int xo = 0, yo = 0, zo = 0;
-			int csize = _csize;
+			byte* octree = wholeoctree;
 			int globalid = 0;
 			int depth = 1;
-			int layerindex = 1;
 			int pow8 = 1;
+			float dist = 0xffffff;
+			
+			for (int chunkx = 0; chunkx < chunk_count; chunkx++) {
+				for (int chunky = 0; chunky < chunk_count; chunky++) {
+					for (int chunkz = 0; chunkz < chunk_count; chunkz++) {
+						octree = wholeoctree + chunk_size * (chunkx + chunk_count * chunky + chunk_count * chunk_count * chunkz);
+						glm::vec3 chunkoff = { chunkx * _csize, chunky * _csize, chunkz * _csize };
 
-			for (int d = 0; d <= octree_depth; d++) {
-				alt_data[d].mask = 0b11111111;
-			}
+						byte oc = 255;
+						int xo = 0, yo = 0, zo = 0;
+						int csize = _csize;
+						globalid = 0;
+						depth = 1;
+						int layerindex = 1;
+						pow8 = 1;
 
-			for ( ; depth <= octree_depth; depth++) {
-				Data* ad = &(alt_data[depth]);
+						for (int d = 0; d <= octree_depth; d++) {
+							alt_data[d].mask = 0b11111111;
+						}
 
-				ad->globalid = globalid;
-				ad->csize = csize;
-				ad->layerindex = layerindex;
+						for (; depth <= octree_depth; depth++) {
+							Data* ad = &(alt_data[depth]);
 
-				dist = 0xffffff;
-				csize /= 2;
-				globalid = globalid * 8;
+							ad->globalid = globalid;
+							ad->csize = csize;
+							ad->layerindex = layerindex;
 
-				oc = test_octree(bounds, csize, octree, layerindex, ray, xo, yo, zo, 0, globalid, &dist, origin, &(ad->mask));
-				byte oc1 = test_octree(bounds, csize, octree, layerindex, ray, xo, yo + csize, zo, 4, globalid, &dist, origin, &(ad->mask));
+							csize /= 2;
+							globalid = globalid * 8;
 
-				ad->pow8 = pow8;
-				ad->xo = xo;
-				ad->yo = yo;
-				ad->zo = zo;
+							oc = test_octree(bounds, csize, octree, layerindex, ray, xo + chunkoff.x, yo + chunkoff.y, zo + chunkoff.z, 0, globalid, &dist, origin, &(ad->mask));
+							byte oc1 = test_octree(bounds, csize, octree, layerindex, ray, xo + chunkoff.x, yo + csize + chunkoff.y, zo + chunkoff.z, 4, globalid, &dist, origin, &(ad->mask));
 
-				if (oc1 != 255) oc = oc1;
-				globalid += oc;
-				if (oc != 255) {
-					switch (oc) {
-					case 1:
-						xo += csize;
-						break;
+							ad->pow8 = pow8;
+							ad->xo = xo;
+							ad->yo = yo;
+							ad->zo = zo;
 
-					case 2:
-						xo += csize;
-						zo += csize;
-						break;
+							if (oc1 != 255) oc = oc1;
+							globalid += oc;
+							if (oc != 255) {
+								switch (oc) {
+								case 1:
+									xo += csize;
+									break;
 
-					case 3:
-						zo += csize;
-						break;
+								case 2:
+									xo += csize;
+									zo += csize;
+									break;
 
-					case 4:
-						yo += csize;
-						break;
+								case 3:
+									zo += csize;
+									break;
 
-					case 5:
-						xo += csize;
-						yo += csize;
-						break;
+								case 4:
+									yo += csize;
+									break;
 
-					case 6:
-						xo += csize;
-						yo += csize;
-						zo += csize;
-						break;
+								case 5:
+									xo += csize;
+									yo += csize;
+									break;
 
-					case 7:
-						yo += csize;
-						zo += csize;
-						break;
+								case 6:
+									xo += csize;
+									yo += csize;
+									zo += csize;
+									break;
 
-					default:
-						break;
+								case 7:
+									yo += csize;
+									zo += csize;
+									break;
+
+								default:
+									break;
+								}
+								ad->mask &= ~(1 << oc);
+								/*int a = (layerindex + globalid) * 4;
+								//if (octree[a + 3] > 128) {
+									color.r = octree[a];
+									color.g = octree[a + 1];
+									color.b = octree[a + 2];
+								//}*/
+
+								ad->oc = oc;
+								pow8 *= 8;
+								layerindex += pow8;
+							}
+							else {
+								if (alt_data[1].mask == 0 || depth == 1)
+									break;
+
+								ad->mask = 0b11111111;
+								ad = &(alt_data[depth - 1]);
+								pow8 = ad->pow8;
+								layerindex = ad->layerindex;
+								globalid = ad->globalid;
+								csize = ad->csize;
+								xo = ad->xo;
+								yo = ad->yo;
+								zo = ad->zo;
+								depth -= 2;
+							}
+						}
+						const int index = ((1 - pow8) / -7 + globalid) * 4;
+						if (depth >= octree_depth + 1 && octree[index + 3] > 128) {
+							color.r = octree[index];
+							color.g = octree[index + 1];
+							color.b = octree[index + 2];
+						}
 					}
-					ad->mask &= ~(1 << oc);
-					/*int a = (layerindex + globalid) * 4;
-					//if (octree[a + 3] > 128) {
-						color.r = octree[a];
-						color.g = octree[a + 1];
-						color.b = octree[a + 2];
-					//}*/
-
-					ad->oc = oc;
-					pow8 *= 8;
-					layerindex += pow8;				
-				}
-				else { 
-					if(alt_data[1].mask == 0 || depth == 1)
-						break;
-
-					ad->mask = 0b11111111;
-					ad = &(alt_data[depth - 1]);
-					pow8 = ad->pow8;
-					layerindex = ad->layerindex;
-					globalid = ad->globalid;
-					csize = ad->csize;
-					xo = ad->xo;
-					yo = ad->yo;
-					zo = ad->zo;
-					depth -= 2;
 				}
 			}
 
-			const int index = ((1 - pow8) / -7 + globalid) * 4;
+			/*const int index = ((1 - pow8) / -7 + globalid) * 4;
 			if (depth >= octree_depth + 1 && octree[index + 3] > 128) {
 				color.r = octree[index];
 				color.g = octree[index + 1];
 				color.b = octree[index + 2];
-			}
+			}*/
 			setPixel(x, y, (byte)color.x, (byte)color.y, (byte)color.z, img);
 		}
 	}
@@ -383,13 +402,32 @@ void set_voxel(int x, int y, int z, byte* octree, int octree_depth, byte r, byte
 	octree[index + 3] = 255;
 }
 
+void fill_chunk(int csize, byte* octree, int chunk_size, int octree_depth, int chid) {
+	const int cm = 256 / csize;
+	//const int c = csize / 2;
+
+	for (int x = 0; x < csize; x++) {
+		for (int y = 0; y < csize; y++) {
+			for (int z = 0; z < csize; z++) {
+
+				//float A = c - x, B = c - y, C = c - z;
+				//if((x > c + 1 || x < c - 1) && (y > c + 1 || y < c - 1) && sqrt(A * A + B * B + C * C) < 14) {
+				set_voxel(x, y, z, octree + chunk_size * chid, octree_depth, x * cm, y * cm, z * cm, csize);
+				//}
+			}
+		}
+	}
+}
+
 int main(void) {
 
 	bool building = false;
 
+	const int chunk_count = 2;
 	const int octree_depth = 5;
-	const int len = (1 - pow(8, (octree_depth + 1))) / -7;
-	byte* octree = new byte[ len * 4 ]; //Sn = 1 * (1 - q^n) / (1 - q)
+	const int chunk_size = ((1 - pow(8, (octree_depth + 1))) / -7) * 4;
+	const int len = chunk_size * chunk_count * chunk_count * chunk_count;
+	byte* octree = new byte[ len ]; //Sn = 1 * (1 - q^n) / (1 - q)
 
 	int index = 0;
 	for (int i = 0; i <= octree_depth; i++) {
@@ -400,7 +438,7 @@ int main(void) {
 			node[n] = 255;
 			node[n + 1] = 255;
 			node[n + 2] = 255;
-			node[n + 3] = 0;
+			node[n + 3] = 255;
 			/*if (i < octree_depth - 2)
 				node[n + 3] = 255;// randomByte();
 			else
@@ -410,27 +448,11 @@ int main(void) {
 	//std::fill_n(octree, len * 4, 0xff);
 
 	const int csize = std::pow(2, octree_depth);
-	const int cm = 256 / csize;
-	/*for (int x = 0; x < csize; x++)
-		for (int y = 0; y < csize; y++)
-			for (int z = 0; z < csize; z++)
-				set_voxel(x, y, z, octree, octree_depth, x * cm, y * cm, z * cm, csize);*/
-
-	const int c = csize/2;
 	
-	for (int x = 0; x < csize; x++) {
-		for (int y = 0; y < csize; y++) {
-			for (int z = 0; z < csize; z++) {
-
-				float A = c - x, B = c - y, C = c - z;
-				if((x > c + 1 || x < c - 1) && (y > c + 1 || y < c - 1) && sqrt(A * A + B * B + C * C) < 14) {
-					set_voxel(x, y, z, octree, octree_depth, x * cm, y * cm, z * cm, csize);
-				}
-			}
-		}
+	for (int i = 0; i < 8; i++) {
+		fill_chunk(csize, octree, chunk_size, octree_depth, i);
 	}
-
-
+	
 
 	// print cwd, nice for debugging
 	{
@@ -490,7 +512,7 @@ int main(void) {
 
 
 		origin = camera.update(window, &rot);
-		draw(width, height, img, &region, &origin, &rot, octree, octree_depth, csize);
+		draw(width, height, img, &region, &origin, &rot, octree, octree_depth, csize, chunk_count, chunk_size);
 
 		// clear the screen and depth buffer
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
